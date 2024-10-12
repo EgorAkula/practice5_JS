2969b02c74bd7e4fb6a0acc24bf99364801723ca
EgorAkula <147970616+EgorAkula@users.noreply.github.com>
2024-10-12 14:50:47 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+//  N1
+let myName = `Egor`;
+let mySurname = `Akulenok`;
+console.log(myName);
+console.log(mySurname);
+// N2
+let str = '!!!';
+console.log(str);
+// N3
+let text1 = `java`;
+let text2 = `script`;
+console.log(text1 + text2);
+// N4
+let greetings1 = `hello`;
+let greetings2 = ` world`;
+console.log(greetings1 + greetings2);
+// N5
+let example = `Как уже неоднократно упомянуто, акционеры крупнейших компаний набирают популярность среди определенных слоев населения, а значит, должны быть описаны максимально подробно.`;
+console.log(`переменная example имеет ${example.length} строк`);
+// N6
+let str1 = `xxx`;
+let str2 = `yyy`;
+let txt = `aaa ${str1} bbb ${str2} ccc`;
+console.log(txt);
+// N7
+str = `a
+b
+c`
+console.log(str);
+// N8
+let x = 46;
+x = String(x);
+console.log(`${x.length}`);
+// N9
+let y = 40;
+let b = 727;
+y = String(y);
+b = String(b);
+console.log(`${y.length} ${b.length}`);
+console.log(`${y.length + b.length}`);
